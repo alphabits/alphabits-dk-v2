94ce0ba308bf033b391e4a101d021c752ebe1e09
Anders Hørsted <anders@alphabits.dk>
2012-06-18 21:55:36 +0200

    Finished blog post about keeping code dry

--- aw/modules/blog/views.py
+++ aw/modules/blog/views.py
@@ -12,6 +12,12 @@ def handle_post_date(post):
     post['date'] = parse_date_string(post['date'])
     return post
 
+def get_posts_to_show():
+    if 'show_all' in request.args:
+        return all_posts
+    else:
+        return [p for p in all_posts if p['status'] == 'published']
+
 all_posts = json.load(open(join_path(blog.root_path, 'posts.json'), 'r'))
 all_posts = map(handle_post_date, all_posts)
 
@@ -19,10 +25,17 @@ all_posts = map(handle_post_date, all_posts)
 @blog.route('/')
 @blog.route('/page/<int:page_num>')
 def index(page_num=1):
-    if 'show_all' in request.args:
-        posts = all_posts
-    else:
-        posts = [p for p in all_posts if p['status'] == 'published']
+    posts = get_posts_to_show()
+    return show_paginated_posts(posts, page_num)
+
+@blog.route('/tags/<tag_name>')
+@blog.route('/tags/<tag_name>/page/<int:page_num>')
+def tag_index(tag_name, page_num=1):
+    posts = [p for p in get_posts_to_show() if tag_name in p["tags"]]
+    return show_paginated_posts(posts, page_num)
+
+
+def show_paginated_posts(posts, page_num):
     pagination = Pagination(posts, page_num, BLOG_PAGE_SIZE)
     if not pagination.current_page_exists:
         abort(404)
